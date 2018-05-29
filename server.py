606586aaf99d4071f38c1d6d83ce11bb0e6a3259
Chia-An <chia-an.yu@epfl.ch>
2018-05-29 15:04:20 +0200

    implement follow-up search, change data format

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 import json
 from flask import Flask, render_template, request
 import random
-import time
 
 
 app = Flask(__name__)
@@ -21,27 +20,64 @@ def get_data():
     """
 
     # Parameters for SQl queries is given by the POST method
-    print(request.form['query_id'])
+    print(request.form)
 
-    # The query result is returned in JSON format:
-    # 'columns': column name.
-    #            [{'title': COLUMN_1_NAME},
-    #             {'title': COLUMN_2_NAME}, ...]
-    # 'data': query result.
-    #         [[ROW_1_COLUMN_1, ROW_1_COLUMN_2, ...],
-    #          [ROW_2_COLUMN_1, ROW_2_COLUMN_2, ...], ...]
-    n_cols = 3
+    # The query/search results is returned in JSON format:
+    # 'data': [{FIELD_1: VALUE_1_1, FIELD_2: VALUE_1_2, ...},
+    #          {FIELD_1: VALUE_2_1, FIELD_2: VALUE_2_2, ...}, ...]
+    #         To support follow-up search (for some entries),
+    #         'personId' or 'filmId' as a field should be included in those dicts.
+    # 'columns': a list of dictionaries
+    #            Required fields:
+    #                'title': title displayed on table
+    #                'data': field name in data
+    #            Optional fields:
+    #                'idType': for follow-up search,
+    #                           should be 'person' or 'film'
+    n_cols = 5
     n_data = 10
-    cols = [{'title': 'column_' + str(i + 1)}
+
+    # basic table
+    cols = [{'title': 'column_' + str(i + 1),
+             'data': 'column_' + str(i + 1)}
             for i in range(n_cols)]
-    data = [[0 * i + col + 1 + random.random()
-              for col in range(n_cols)]
+    data = [{cols[col]['data']: 0 * i + col + 1 + random.random()
+             for col in range(n_cols)}
             for i in range(n_data)]
-    json_data = {'columns': cols,
-                 'data': data}
+
+    # add ids
+    [p, f] = random.sample(list(range(n_cols)), k=2)
+    cols[p]['idType'] = 'person'
+    cols[f]['idType'] = 'film'
+    for row in data:
+        row['personId'] = random.random()
+        row['filmId'] = random.random()
+
+    json_data = {'columns': cols, 'data': data}
     print(json_data)
 
     return json.dumps(json_data)
+
+
+@app.route('/_get_info', methods=["POST"])
+def get_info():
+    """
+    This function is for follow-up info on a person or a film.
+    The request form has the form {'p': personId} or {'f': filmId}.
+    """
+
+    print(request.form)
+
+    if 'p' in request.form:
+        ret = 'personID: ' + request.form['p']
+    elif 'f' in request.form:
+        ret = 'filmID: ' + request.form['f']
+    else:
+        assert 0
+
+    ret += '\n\nWrite some information here.'
+
+    return ret
 
 
 if __name__ == "__main__":
